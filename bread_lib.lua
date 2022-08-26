68f3ac474d49108c4df6844efe2540772b9aad70
bread-with-toast <108555960+bread-with-toast@users.noreply.github.com>
2022-08-26 02:28:20 +0100

    Add files via upload

--- bread_lib.lua
+++ bread_lib.lua
@@ -1,7 +1,7 @@
 math.randomseed(os.time() + 3924 * 3)
 
 bread = {}
-variables = {}
+toast = {}
 
 function bread.eat_toast(text)
     print(text)
@@ -24,12 +24,50 @@ function bread.eat(x, y)
 end
 
 function bread.make_bread(key, value)
-    variables[key] = value
+    toast[key] = value
 end
 
 function bread.toast_bread()
     value = math.random(0, 1)
-    print(value)
+    if value == 1 then
+        return True
+    else
+        return False
+    end
+end
+
+function bread.form(question)
+    io.write(question .. "\n")
+    input = io.read()
+    return input
+end
+
+function bread.cook_bread(key)
+    return toast[key]
+end
+
+function bread.halt(seconds)
+    time = os.time()
+    repeat until time + seconds == os.time()
+end
+
+function bread.error(text)
+    print("\x1b[41m Error: " .. text .. " \x1b[0m")
+end
+
+function bread.warning(text)
+    print("\x1b[43m Warning: " .. text .. " \x1b[0m")
+end
+
+function bread.success(text)
+    print("\x1b[42m Success: " .. text .. " \x1b[0m")
+end
+
+function bread.taste(assertion, command)
+    if assertion then
+        return command
+    end
+
 end
 
 return bread
